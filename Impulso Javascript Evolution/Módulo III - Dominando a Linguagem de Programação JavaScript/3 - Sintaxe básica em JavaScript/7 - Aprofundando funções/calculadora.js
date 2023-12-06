e35c528b83f9846286dd73fdb59b686bb93bf57e
Gabriel <gabrielnmatos@gmail.com>
2023-12-06 18:48:05 -0300

    sintaxe basico em javascript comcluido

--- Impulso Javascript Evolution/Módulo III - Dominando a Linguagem de Programação JavaScript/3 - Sintaxe básica em JavaScript/7 - Aprofundando funções/calculadora.js
+++ Impulso Javascript Evolution/Módulo III - Dominando a Linguagem de Programação JavaScript/3 - Sintaxe básica em JavaScript/7 - Aprofundando funções/calculadora.js
@@ -11,46 +11,53 @@ function calculadora() {
     }
   }
   const operacao = Number(prompt('Escolha uma operação:\n1 - Soma (+)\n2 - Subtração (-)\n3 - Multiplicação (*)\n4 - Divisão (/)\n5 - Divisão Inteira/Resto da Divisão (%)\n6 - Potenciação (**)')); // abre uma caixa de input no navegador
-  if (operacao >= 7) {
+  if (operacao >= 7 || ! operacao) {
     alert('digite uma opção válida');
     calculadora();
   }
   let n1 = Number(prompt('Insira o primeiro número:'));
-  if (typeof(n1) != 'number' || isNaN(n1)) {
+  if (typeof(n1) != 'number' || isNaN(n1) || ! n1) {
     alert('digite uma opção válida');
     calculadora();
   }
   let n2 = Number(prompt('Insira o segundo número:'));
-  if (typeof(n2) != 'number' || isNaN(n1)) {
+  if (typeof(n2) != 'number' || isNaN(n2) || ! n2) {
     alert('digite uma opção válida');
     calculadora();
   }
   let resultado; 
 
-  if (operacao === 1) {
-    resultado = n1 + n2;
-    alert(`${n1} + ${n2} = ${resultado}`);
-    nova_operacao();
-  } else if (operacao === 2) {
-    resultado = n1 - n2;
-    alert(`${n1} - ${n2} = ${resultado}`);
-    nova_operacao();
-  } else if (operacao === 3) {
-    resultado = n1 * n2;
-    alert(`${n1} * ${n2} = ${resultado}`);
-    nova_operacao();
-  } else if (operacao === 4) {
-    resultado = n1 / n2;
-    alert(`${n1} / ${n2} = ${resultado}`);
-    nova_operacao();
-  } else if (operacao === 5) {
-    resultado = n1 % n2;
-    alert(`${n1} % ${n2} = ${resultado}`);
-    nova_operacao();
-  } else if (operacao === 6) {
-    resultado = n1 ** n2;
-    alert(`${n1} ** ${n2} = ${resultado}`);
-    nova_operacao();
+  switch (operacao) {
+    case 1:
+      resultado = n1 + n2;
+      alert(`${n1} + ${n2} = ${resultado}`);
+      nova_operacao();
+      break
+    case 2:
+      resultado = n1 - n2;
+      alert(`${n1} - ${n2} = ${resultado}`);
+      nova_operacao();
+      break
+    case 3:
+      resultado = n1 * n2;
+      alert(`${n1} * ${n2} = ${resultado}`);
+      nova_operacao();
+      break
+    case 4:
+      resultado = n1 / n2;
+      alert(`${n1} / ${n2} = ${resultado}`);
+      nova_operacao();
+      break
+    case 5:
+      resultado = n1 % n2;
+      alert(`${n1} % ${n2} = ${resultado}`);
+      nova_operacao();
+      break
+    case 6:
+      resultado = n1 ** n2;
+      alert(`${n1} ** ${n2} = ${resultado}`);
+      nova_operacao();
+      break
   }
 }
 
