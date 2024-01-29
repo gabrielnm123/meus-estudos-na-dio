8feac89e6c3e145ff41c1e033e5a2a50e23e9974
gabrielnm123 <gabrielnmatos96@gmail.com>
2024-01-29 06:33:38 -0300

    parte 2 começando

---
+++ Impulso Javascript Evolution/Módulo IV - Front-end: Desenvolvimento Web com React/2 - Introdução ao ReactJS/02 - Integrando webpack ao desenvolvimento com ReactJS/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <p>Digital Innovation One</p>
+      <p>Bem vindo a nossa aula =D.</p>
+      <p>webpack 4</p>
+      <p>testando 1 2</p>
+    </div>
+  )
+}
+
+export default App;
